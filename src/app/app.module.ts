54a747cadd20b0676982d9272c2e42d7a3a6c51e
fabiocaiulo <info.fabiocaiulo@gmail.com>
2022-04-21 09:33:24 +0200

    Implemented Responsive Sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +25,8 @@ import { SlidesComponent } from './slides/slides.component';
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
     AppRoutingModule
   ],
   providers: [],
